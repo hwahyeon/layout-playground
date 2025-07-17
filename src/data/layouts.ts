b98ee04affee2ffa0925b87398922fb2167c13b3
Janghyeon <hwahyeon.dev@gmail.com>
2025-07-17 14:45:10 +0200

    feat: Upload layout metadata

---
+++ src/data/layouts.ts
@@ -0,0 +1,22 @@
+export interface LayoutMeta {
+  id: string;
+  name: string;
+  responsive: boolean;
+}
+
+export const layouts: LayoutMeta[] = [
+  { id: "01", name: "Layout 01", responsive: true },
+  { id: "02", name: "Layout 02", responsive: false },
+  { id: "03", name: "Layout 03", responsive: false },
+  { id: "04", name: "Layout 04", responsive: false },
+  { id: "05", name: "Layout 05", responsive: false },
+  { id: "06", name: "Layout 06", responsive: false },
+  { id: "07", name: "Layout 07", responsive: false },
+  { id: "08", name: "Layout 08", responsive: false },
+  { id: "09", name: "Layout 09", responsive: false },
+  { id: "10", name: "Layout 10", responsive: false },
+  { id: "11", name: "Layout 11", responsive: true },
+  { id: "12", name: "Layout 12", responsive: true },
+  { id: "13", name: "Layout 13", responsive: true },
+  { id: "14", name: "Layout 14", responsive: true },
+];
